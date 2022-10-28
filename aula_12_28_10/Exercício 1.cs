b9ea841701556c225fca7cd2a50f631b48c8e3b7
andrevpc <andrevictor06@gmail.com>
2022-10-28 11:56:26 -0300

    Exercícios

---
+++ aula_12_28_10/Exercício 1.cs
@@ -0,0 +1,53 @@
+﻿// using System;
+
+// for (int j = 0; j < 3; j++)
+// {
+//     ListarPessoas.Adicionar(ListarPessoas.Criar());
+// }
+
+// ListarPessoas.Maior();
+
+// public class Pessoa
+// {
+//     public int Idade { get; set; }
+//     public string Nome  { get; set; }
+
+//     public Pessoa(int ida, string nome)
+//     {
+//         this.Idade = ida;
+//         this.Nome = nome;
+//     }
+//     public virtual void ExibirDados()
+//     {
+//         Console.WriteLine(this.Idade);
+//         Console.WriteLine(this.Nome);
+//     }
+// }
+
+// public static class ListarPessoas
+// {
+//     public static List<Pessoa> pessoas = new List<Pessoa>();
+//     public static Pessoa MaisVelho = new Pessoa(0, "");
+//     public static Pessoa Criar()
+//     {
+//         Console.WriteLine("Insira o nome da pessoa: ");
+//         string nome = Console.ReadLine();
+//         Console.WriteLine("Insira a idade da pessoa: ");
+//         int ida = int.Parse(Console.ReadLine());
+
+//         return new Pessoa(ida, nome);
+//     }
+//     public static void Adicionar(Pessoa pes)
+//     {
+//         pessoas.Add(pes);
+//     }
+//     public static void Maior()
+//     {
+//         foreach (var p in pessoas)
+//         {
+//             if (p.Idade >= MaisVelho.Idade)
+//                 MaisVelho = p;
+//         }
+//         Console.WriteLine(MaisVelho.Nome);
+//     }
+// }
