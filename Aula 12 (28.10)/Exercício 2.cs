1afce2b7bbb3bfe30138ab71743dff316de24fe4
andrevpc <andrevictor06@gmail.com>
2022-11-01 11:49:47 -0300

    Exercicio 3

--- Aula 12 (28.10)/Exercício 2.cs
+++ Aula 12 (28.10)/Exercício 2.cs
@@ -1,34 +1,34 @@
-using System;
+// using System;
 
-Console.WriteLine("Insira a altura: ");
-int alt = int.Parse(Console.ReadLine());
-Console.WriteLine("Insira a base: ");
-int bas = int.Parse(Console.ReadLine());
+// Console.WriteLine("Insira a altura: ");
+// int alt = int.Parse(Console.ReadLine());
+// Console.WriteLine("Insira a base: ");
+// int bas = int.Parse(Console.ReadLine());
 
-Retangulo retangulo = new Retangulo(alt, bas);
-retangulo.VerArea();
+// Retangulo retangulo = new Retangulo(alt, bas);
+// retangulo.VerArea();
 
-public class Retangulo
-{
-    public int Altura { get; set; }
-    public int Base { get; set; }
-    public Retangulo(int alt, int bas)
-    {
-        this.Altura = alt;
-        this.Base = bas;
-    }
-    public float Area()
-    {
-        return this.Altura * this.Base;
-    }
-    public void VerArea()
-    {
-        Console.WriteLine("Área: "+this.Area());
-    }
-    public void Propriedades()
-    {
-        Console.WriteLine("Altura: "+this.Altura);
-        Console.WriteLine("Base: "+this.Base);
-        Console.WriteLine("Área: "+this.Area());
-    }
-}
+// public class Retangulo
+// {
+//     public int Altura { get; set; }
+//     public int Base { get; set; }
+//     public Retangulo(int alt, int bas)
+//     {
+//         this.Altura = alt;
+//         this.Base = bas;
+//     }
+//     public float Area()
+//     {
+//         return this.Altura * this.Base;
+//     }
+//     public void VerArea()
+//     {
+//         Console.WriteLine("Área: "+this.Area());
+//     }
+//     public void Propriedades()
+//     {
+//         Console.WriteLine("Altura: "+this.Altura);
+//         Console.WriteLine("Base: "+this.Base);
+//         Console.WriteLine("Área: "+this.Area());
+//     }
+// }
